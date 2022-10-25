28be13abdb8f4d1d080266f9f655d662c7f5f3b0
mlk <masoud371malek@gmail.com>
2022-10-25 11:17:17 +0330

    add container to project

---
+++ message/urls.py
@@ -0,0 +1,10 @@
+# from django.urls import path, include
+# from django.contrib import admin
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from message.views import index, room
+# from django.contrib.auth import views
+# urlpatterns = [
+#     path('', index, name='message_index'),
+#     path("<str:room_name>/", room, name='room_index'),
+# ]
